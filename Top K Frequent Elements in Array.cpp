ef58a4d88535cf1911c22ccdd1ed6f0ddc2daad1
Ravi Ranjan <67729699+raviranjan940@users.noreply.github.com>
2023-11-05 00:53:58 +0530

    Create Top K Frequent Elements in Array.cpp

---
+++ Top K Frequent Elements in Array.cpp
@@ -0,0 +1,16 @@
+vector<int> topK(vector<int>& nums, int k) {
+        vector<pair<int, int>>v;
+        unordered_map<int, int>mp;
+        vector<int>ans;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        for(auto x: mp){
+            v.push_back({x.second, x.first});
+        }
+        sort(v.rbegin(), v.rend());
+        for(int i=0; i<k; i++){
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
