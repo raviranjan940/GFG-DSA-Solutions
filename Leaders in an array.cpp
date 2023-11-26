8214b84703232b2e5598409555b2d096d47b114d
Ravi Ranjan <67729699+raviranjan940@users.noreply.github.com>
2023-11-26 22:25:32 +0530

    Create Leaders in an array.cpp

---
+++ Leaders in an array.cpp
@@ -0,0 +1,14 @@
+//Function to find the leaders in the array.
+    vector<int> leaders(int a[], int n){
+        vector<int> ans;
+        int localmax = INT_MIN;
+            for(int i = n - 1; i >= 0; i--){
+                localmax = max(localmax, a[i]);
+                if(localmax <= a[i]){
+                    ans.push_back(a[i]);
+                }
+            }
+        reverse(ans.begin() , ans.end());
+        return ans;
+        
+    }
